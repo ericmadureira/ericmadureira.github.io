51cddbba7329aac5b3b3ac9746a5d5d38fc20dd9
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-12-06 15:55:15 -0300

    feat: remove template parts
    Removes unnecessary parts added by the gatsby template.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Eric Madureira | Blog`,
+    description: `A blog about frontend development and career building.`,
+    author: `@ericmadureira`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
