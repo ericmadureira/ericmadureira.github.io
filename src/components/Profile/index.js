07e38d232b3405376031cd5901f956e76016767a
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-12-08 23:22:43 -0300

    feat: avatar pic
    Adds profile pic through the Avatar component.

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
+import Avatar from '../Avatar'
+
 const Profile = () => (
   <div>
+    <Avatar />
     <h1>Eric Madureira</h1>
     <h2>Front-end Developer</h2>
     <p>Thoughts about software development and career building</p>
