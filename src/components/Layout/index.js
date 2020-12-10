47e734a8157217b5fe08840c8112ff95ed4fc78d
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-12-09 22:13:13 -0300

    feat: Layout styled components
    Changes Layout to use styled components from a separate file in the
    same folder.

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,29 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styled from 'styled-components';
 
-import Profile from '../Profile';
 import GlobalStyles from '../../styles/global'
-
-const LayoutWrapper = styled.section`
-  display: flex;
-  flex-direction: column;
-`;
-
-const LayoutMain = styled.main`
-  background-color: #e8e8e8;
-  min-width: 100vh;
-  padding: 0 20rem;
-  width: 100%;
-`;
+import Profile from '../Profile';
+import * as S from './styled';
 
 const Layout = ({ children }) => {
   return (
-    <LayoutWrapper>
+    <S.LayoutWrapper>
       <GlobalStyles />
       <Profile />
-      <LayoutMain>{children}</LayoutMain>
-    </LayoutWrapper>
+      <S.LayoutMain>{children}</S.LayoutMain>
+    </S.LayoutWrapper>
   )
 };
 
---
+++ src/components/Layout/styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const LayoutWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const LayoutMain = styled.main`
+  background-color: #e8e8e8;
+  min-width: 100vh;
+  padding: 0 20rem;
+  width: 100%;
+`;
