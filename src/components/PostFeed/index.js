29be0c9a5f0bbe4c08aa928f934ea99f1dac8cdb
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-12-14 21:52:29 -0300

    feat: mocked post feed improvement
    Adds hardcoded mock info and improves the feed items.

--- src/components/PostFeed/index.js
+++ src/components/PostFeed/index.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 
 import * as S from './styled'
 
+const posts = [
+  { date: 'December 14, 2020', title: 'Post Title', subtitle: 'Post subtitle here' },
+  { date: 'December 14, 2020', title: 'Post Title', subtitle: 'Post subtitle here' },
+  { date: 'December 14, 2020', title: 'Post Title', subtitle: 'Post subtitle here' }
+]
+
 const PostFeed = () => {
+  const mappedPosts = useMemo(() => (
+    posts.map(({ date, subtitle, title }) => (
+      <S.PostLinkWrapper>
+        <S.PostItem>
+          <S.PostTitle>{title}</S.PostTitle>
+          <S.PostDate>{date}</S.PostDate>
+          <S.PostSubtitle>{subtitle}</S.PostSubtitle>
+        </S.PostItem>
+      </S.PostLinkWrapper>
+    ))
+  ), [])
+
   return (
     <S.PostFeedWrapper>
-      Feed goes gere
+      {mappedPosts}
     </S.PostFeedWrapper>
   )
 }
--- src/components/PostFeed/styled.js
+++ src/components/PostFeed/styled.js
@@ -1,6 +1,17 @@
 import styled from 'styled-components'
 
 export const PostFeedWrapper = styled.section`
+  background-color: #fff;
   display: flex;
   flex-direction: column;
 `
+
+export const PostLinkWrapper = styled.a``
+
+export const PostItem = styled.div``
+
+export const PostTitle = styled.p``
+
+export const PostSubtitle = styled.p``
+
+export const PostDate = styled.p``
