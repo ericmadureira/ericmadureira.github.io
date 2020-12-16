0fe9e7f271f4ba9abc7c8ead39bdf87024fde041
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-12-15 23:57:04 -0300

    feat: post list graphql query
    Starts the fetch of local posts using graphql.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,38 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby'
 
 import Layout from '../components/Layout';
 import SEO from '../components/seo';
 import PostFeed from '../components/PostFeed';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title='Home' />
-    <PostFeed />
-  </Layout>
-);
+const IndexPage = () => {
+  const { allMarkdownRemark } = useStaticQuery(graphql`
+    query PostList {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              date
+              description
+              title
+            }
+            timeToRead
+          }
+        }
+      }
+    }
+  `)
+
+  const { postList } = allMarkdownRemark.edges
+
+  postList.map(({ node: { frontmatter: { date, description, title }, timeToRead}}) => date)
+
+  return (
+    <Layout>
+      <SEO title='Home' />
+      <PostFeed />
+    </Layout>
+  )
+}
 
 export default IndexPage;
