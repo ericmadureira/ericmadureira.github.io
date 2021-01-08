215f65457be549dadec62e3e6c4295f30e05e8e3
Eric Madureira <ericmadureira.uesc@gmail.com>
2021-01-07 23:10:01 -0300

    fix: fix path resolve
    Fixes problems while trying to resolve file paths.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
-import SEO from '../components/SEO'
+import SEO from '../components/seo'
 import * as S from '../components/Post/styled'
 
 const BlogPost = ({ data }) => {
