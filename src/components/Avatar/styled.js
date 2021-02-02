7b2aa4aea8ddd68e81cb2c0fadb74dad32d1e314
Eric Madureira <ericmadureira.uesc@gmail.com>
2021-02-02 19:28:38 -0300

    Updates

--- src/components/Avatar/styled.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-import Img from 'gatsby-image';
-
-export const AvatarWrapper = styled(Img)`
-  border-radius: 50%;
-  height: 3.75rem;
-  width: 3.75rem;
-`
--- src/components/SocialLinks/icons.js
+++
@@ -1,9 +0,0 @@
-import { Github } from '@styled-icons/boxicons-logos/Github'
-import { LinkedinSquare } from '@styled-icons/boxicons-logos/LinkedinSquare'
-
-const Icons = {
-  Github,
-  LinkedinSquare
-}
-
-export default Icons
